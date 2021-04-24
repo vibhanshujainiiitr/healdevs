3fa73277bfe000cd27d53bc4fbcea81d7b93bcd0
Suyash Patil <suyashpatil532001@gmail.com>
2021-04-25 00:24:29 +0530

    doctor form initiated

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebase'
 
-const Doctor = () => {
+const Doctor = ({ history, user, setUser }) => {
+
   return (
     <div>
-
+      <div>
+        <div><h2>This is the doctor name</h2></div>
+        <p>Patient Email</p>
+        <p>Patient diary</p>
+      </div>
     </div>
   )
 }
 
 export default Doctor
+
