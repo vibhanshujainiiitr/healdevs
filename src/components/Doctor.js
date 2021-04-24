7dd3ed9d0522aec64c1930b4fa1800b675ae92a9
Suyash Patil <suyashpatil532001@gmail.com>
2021-04-25 00:44:28 +0530

    doctor functionality added

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -3,12 +3,36 @@ import { db } from '../firebase'
 
 const Doctor = ({ history, user, setUser }) => {
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      if (user.email === "pullstackdevelopers@gmail.com") {
+        db.collection("questions").orderBy("timestamp", "desc")
+          .onSnapshot((snapshot) => {
+            setData(snapshot.docs.map(doc => ({
+              id: doc.id,
+              userData: doc.data()
+            })))
+          })
+      }
+    }
+  }, [user])
+
   return (
     <div>
       <div>
         <div><h2>This is the doctor name</h2></div>
-        <p>Patient Email</p>
-        <p>Patient diary</p>
+        {
+          data.map(({ userData, id }) => (
+            <div>
+              <div>
+                <p>{userData.email}</p>
+                <p>How are you feeling today?<br/>{userData.dailyDiary}</p>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
