06672c5fe2dfb79186730c5c9349c0828b75f72f
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2021-04-25 09:03:16 +0530

    Landing Page added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Navbar from './Navbar'
+import './landing.css';
+import { Link } from 'react-router-dom'
+import { Wave } from 'react-animated-text';
 
 const Landing = () => {
   return (
     <div>
-      This is Landing
-      <Navbar/>
+      <div class="landing-home">
+        <div class="container">
+          <div class="first-content">
+            <h1>Mental Therapy Space</h1>
+            <span><em>By</em> HealDevs</span>
+            <a class="fa fa-angle-down page-scroll" href="#about"></a>
+            <div className="button">
+              <Link to='/login' className="Login">
+                Start
+            </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
